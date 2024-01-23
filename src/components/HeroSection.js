e197fadcb5e55d59cd08f45a8c04b97b8d8163f7
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-01-23 14:22:53 +0200

    hero

---
+++ src/components/HeroSection.js
@@ -0,0 +1,34 @@
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row">
+        <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
+          <h1 className=" max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
+            Bring everyone together to build better products
+          </h1>
+
+          <p className=" text-darkGrayishBlue max-w-sm text-center md:text-left">
+            Manage makes it simple for software teams to plan day-to-day tasks
+            while keeping the larger team goals in view.
+          </p>
+
+          <div class="flex justify-center md:justify-start">
+            <a
+              href="#"
+              className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+            >
+              Get Started
+            </a>
+          </div>
+        </div>
+        
+        {/* Right part */}
+        <div className="md:w-1/2">
+          <img src="/illustration-intro.svg" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,36 @@
-
 const Navbar = () => {
   return (
     <nav className=" relative container mx-auto bg-slate-300 p-6">
-        <div className="flex items-center justify-between">
-            <div className="pt-2">
-                <img src="/logo.svg" alt="logo" />
-            </div>
-
-            <div className="hidden md:flex space-x-6">
-                <a href="#" className="hover:text-darkGrayishBlue">Pricing</a>
-                <a href="#" className="hover:text-darkGrayishBlue">Product</a>
-                <a href="#" className="hover:text-darkGrayishBlue">About Us</a>
-                <a href="#" className="hover:text-darkGrayishBlue">Careers</a>
-                <a href="#" className="hover:text-darkGrayishBlue">Community</a>
-            </div>
-
-            <button className="hidden md:block bg-brightRed p-3 px-6 py-2 rounded-full text-white hover:bg-brightRedLight">Get Started</button>
+      <div className="flex items-center justify-between">
+        <div className="pt-2">
+          <img src="/logo.svg" alt="logo" />
         </div>
+        <div className="hidden md:flex space-x-6">
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Pricing
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Product
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            About Us
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Careers
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Community
+          </a>
+        </div>
+        <a
+          href="#"
+          class="hidden p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight md:block"
+        >
+          Get Started
+        </a>{" "}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
